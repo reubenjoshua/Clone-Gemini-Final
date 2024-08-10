43f6e7a39a0adce14b7b6042fe81219dfc4edccf
reubenjoshua <rubspalo@gmail.com>
2024-08-11 01:03:32 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from './components/Sidebar/sidebar'
+import Main from './components/Sidebar/Main/Main'
+
+
+const App = () => {
+  return (
+    <>
+      <Sidebar/>
+      <Main/>
+    </>
+  )
+}
+
+export default App
